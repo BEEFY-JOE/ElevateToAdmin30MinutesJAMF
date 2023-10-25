11954b6f04fc0a34fd587ebbcee96507255373c9
BEEFY JOE <joeberardis@gmail.com>
2023-10-25 15:39:39 -0600

    Update MakeMeAnAdmin.sh
    Summary:
    
    Implemented an extensive update to the temporary admin rights script designed for JAMF Self Service. This revision introduces modern macOS commands, a comprehensive logging system, efficient cleanup processes, and a user confirmation mechanism.
    
    Details:
    
    Command Modernization:
    
    Replaced deprecated launchctl load and launchctl unload commands with their modern counterparts launchctl bootstrap and launchctl bootout respectively. This change ensures compatibility with current and future macOS versions.
    Enhanced User Interaction:
    
    Incorporated a user confirmation dialog, which requires users to actively agree to receive temporary admin rights. This step is crucial for ensuring users are conscious of the elevated permissions they're receiving and the associated responsibilities.
    Advanced Logging:
    
    Integrated a sophisticated logging mechanism that records all administrative activities during the temporary elevation period. The logs are compressed for efficiency, and older log archives are auto-purged after six months, ensuring a clean storage environment.
    Streamlined Cleanup:
    
    The new script detects and cleans up any lingering instances of the LaunchDaemon and the removeAdmin script. This proactive cleanup ensures no conflicts or residues from previous runs, promoting a cleaner and conflict-free environment.
    Optimized User Identification:
    
    Rather than relying on the who command, the script is now designed to receive the username directly as a parameter, enhancing accuracy and consistency.
    Conclusion:
    
    With these improvements, the script is better suited for contemporary macOS environments and offers a more secure and efficient experience for JAMF Self Service users. These updates prioritize user awareness, system cleanliness, and overall security.

--- MakeMeAnAdmin.sh
+++ MakeMeAnAdmin.sh
@@ -3,8 +3,8 @@
 ###############################################
 # This script will provide temporary admin    #
 # rights to a standard user right from self   #
-# service. First it will grab the username of #
-# the logged in user, elevate them to admin   #
+# service. First, it will grab the username   #
+# of the logged-in user, elevate them to admin#
 # and then create a launch daemon that will   #
 # count down from 30 minutes and then create  #
 # and run a secondary script that will demote #
@@ -15,73 +15,129 @@
 ###############################################
 
 #############################################
-# find the logged in user and let them know #
+# Check if parameter $3 is passed and set   #
 #############################################
 
-currentUser=$(who | awk '/console/{print $1}')
-echo $currentUser
+if [ -z "$3" ]; then
+    echo "Error: No user specified. \$3 is empty. Exiting."
+    exit 1
+fi
 
-osascript -e 'display dialog "You now have administrative rights for 30 minutes. DO NOT ABUSE THIS PRIVILEGE..." buttons {"Make me an admin, please"} default button 1'
+echo $3
+
+# Display the dialog with a 60-second timeout
+userChoice=$(osascript -e 'display dialog "You will be granted administrative rights for 30 minutes, after which you will be reverted to a standard user. Activity during this period will be logged. Please use this privilege responsibly. If you do not click '"'"'Agree'"'"' within 60 seconds, you will remain a standard user." buttons {"Agree"} default button 1 giving up after 60')
+
+# Check if the user agreed or the dialog timed out
+if [[ $userChoice == *"Agree"* ]]; then
+    echo "User agreed to admin elevation."
+else
+    echo "User did not confirm admin elevation or time expired. Exiting."
+    exit 1
+fi
 
 #########################################################
-# write a daemon that will let you remove the privilege #
-# with another script and chmod/chown to make 			#
-# sure it'll run, then load the daemon					#
+# Write a daemon that will let you remove the privilege #
+# with another script, then load the daemon             #
 #########################################################
 
-#Create the plist
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist Label -string "removeAdmin"
+# Path to the LaunchDaemon plist
+launchDaemonPath="/Library/LaunchDaemons/removeAdmin.plist"
 
-#Add program argument to have it run the update script
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist ProgramArguments -array -string /bin/sh -string "/Library/Application Support/JAMF/removeAdminRights.sh"
+# Check if the LaunchDaemon plist already exists, and if so, bootout and remove it
+if [ -f "$launchDaemonPath" ]; then
+    launchctl bootout system/removeAdmin
+    rm -f "$launchDaemonPath"
+fi
 
-#Set the run inverval to run every 7 days
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist StartInterval -integer 1800
+# Create the LaunchDaemon plist using PlistBuddy
+/usr/libexec/PlistBuddy \
+    -c "Add :Label string removeAdmin" \
+    -c "Add :ProgramArguments array" \
+    -c "Add :ProgramArguments:0 string /bin/bash" \
+    -c "Add :ProgramArguments:1 string /Library/Application Support/JAMF/removeAdminRights.sh" \
+    -c "Add :RunAtLoad bool true" \
+    -c "Add :StartInterval integer 1800" \
+    "$launchDaemonPath"
 
-#Set run at load
-sudo defaults write /Library/LaunchDaemons/removeAdmin.plist RunAtLoad -boolean yes
+# Set ownership and permissions
+chown root:wheel "$launchDaemonPath"
+chmod 644 "$launchDaemonPath"
 
-#Set ownership
-sudo chown root:wheel /Library/LaunchDaemons/removeAdmin.plist
-sudo chmod 644 /Library/LaunchDaemons/removeAdmin.plist
-
-#Load the daemon 
-launchctl load /Library/LaunchDaemons/removeAdmin.plist
+# Bootstrap the daemon 
+launchctl bootstrap system "$launchDaemonPath"
 sleep 10
 
 #########################
-# make file for removal #
+# Make file for removal #
 #########################
 
 if [ ! -d /private/var/userToRemove ]; then
-	mkdir /private/var/userToRemove
-	echo $currentUser >> /private/var/userToRemove/user
-	else
-		echo $currentUser >> /private/var/userToRemove/user
+    mkdir /private/var/userToRemove
+    echo $3 | tee /private/var/userToRemove/user
+else
+    echo $3 | tee -a /private/var/userToRemove/user
 fi
 
 ##################################
-# give the user admin privileges #
+# Give the user admin privileges #
 ##################################
 
-/usr/sbin/dseditgroup -o edit -a $currentUser -t user admin
+/usr/sbin/dseditgroup -o edit -a $3 -t user admin
 
 ########################################
-# write a script for the launch daemon #
+# Write a script for the launch daemon #
 # to run to demote the user back and   #
 # then pull logs of what the user did. #
 ########################################
 
-cat << 'EOF' > /Library/Application\ Support/JAMF/removeAdminRights.sh
-if [[ -f /private/var/userToRemove/user ]]; then
-	userToRemove=$(cat /private/var/userToRemove/user)
-	echo "Removing $userToRemove's admin privileges"
-	/usr/sbin/dseditgroup -o edit -d $userToRemove -t user admin
-	rm -f /private/var/userToRemove/user
-	launchctl unload /Library/LaunchDaemons/removeAdmin.plist
-	rm /Library/LaunchDaemons/removeAdmin.plist
-	log collect --last 30m --output /private/var/userToRemove/$userToRemove.logarchive
+# Define the path for the removal script
+removalScriptPath="/Library/Application Support/JAMF/removeAdminRights.sh"
+
+# If the removal script already exists, delete it before proceeding
+if [ -f "$removalScriptPath" ]; then
+    echo "Old removal script found. Deleting..."
+    rm -f "$removalScriptPath"
+fi
+
+# Now, create the new removal script
+cat << 'EOF' | tee "$removalScriptPath"
+#!/bin/bash
+
+if [ -f /private/var/userToRemove/user ]; then
+    userToRemove=$(cat /private/var/userToRemove/user)
+    echo "Removing $userToRemove's admin privileges"
+    /usr/sbin/dseditgroup -o edit -d "$userToRemove" -t user admin
+    rm -f /private/var/userToRemove/user
+
+    # Define a log file location
+    logFile="/private/var/userToRemove/scriptActivity.log"
+
+    # Get the current time in UTC
+    currentTime=$(date -u +"%Y-%m-%d_%H-%M-%S")
+
+    # Cleanup old logarchive tarballs older than 6 months
+    find /private/var/userToRemove/ -name "*.tar.gz" -type f -mtime +180 -exec rm {} \;
+
+    # Collect logs and apply the current time to the log archive's name
+    log collect --last 30m --output "/private/var/userToRemove/${userToRemove}_UTC_${currentTime}.logarchive"
+
+    # Compress the collected log file
+    tar -czf "/private/var/userToRemove/${userToRemove}_UTC_${currentTime}.logarchive.tar.gz" -C "/private/var/userToRemove" "${userToRemove}_UTC_${currentTime}.logarchive"
+
+    # Remove the original uncompressed logarchive
+    rm -rf "/private/var/userToRemove/${userToRemove}_UTC_${currentTime}.logarchive"
+
+    # Clean up
+    launchDaemonPath="/Library/LaunchDaemons/removeAdmin.plist" 
+    rm "$launchDaemonPath"
+    rm -- "$0"
+
+    # Properly terminate the LaunchDaemon session
+    launchctl bootout system/removeAdmin
 fi
 EOF
+
+chmod +x "$removalScriptPath"
 
 exit 0
